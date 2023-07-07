a3476d431f796b261eb059ae619e527846eadb2e
jonaw <jonaw@Jonathan>
2023-07-07 17:30:02 -0600

    Proyecto

---
+++ src/main/java/com/proyectofinal/domain/Producto.java
@@ -0,0 +1,52 @@
+package com.proyectofinal.domain;
+
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import lombok.Data;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="producto")
+public class Producto implements Serializable {
+    
+    private static final long serialVersionUTD = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name="id_producto")
+    private Long idProducto;
+    private String descripcion;
+    private String detalle;
+    private double precio;
+    private int existencias;
+    private String rutaImagen;
+    private boolean activo; 
+    
+    @ManyToOne
+    @JoinColumn(name= "id_categoria")
+    Categoria categoria;
+    
+    public Producto(){
+        
+    }
+    public Producto(String descripcion, String detalle, double precio, int existencias, String imagen, boolean activo){
+        this.descripcion= descripcion;
+        this.detalle= detalle;
+        this.precio= precio;
+        this.existencias= existencias;
+        this.rutaImagen= rutaImagen;
+        this.activo= activo;
+    }
+}

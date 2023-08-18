1c2eefb22b1aeca9bafe5083536b0fb68daf9360
jonaw <jonaw@Jonathan>
2023-08-17 22:16:26 -0600

    Edicion de Jonathan

---
+++ src/main/java/com/proyectofinal/service/ReporteService.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/springframework/Repository.java to edit this template
+ */
+package com.proyectofinal.service;
+
+import java.io.IOException;
+import java.util.Map;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+
+public interface ReporteService {
+    public ResponseEntity<Resource> generaReporte (
+             String reporte,
+             Map<String, Object> parametros,
+             String tipo
+    ) throws IOException;
+    
+}
